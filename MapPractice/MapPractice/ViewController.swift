fbdb0ffdaeff0b415a59a63b7df19ae1642b1a5a
suyeon <rlatndus9911@naver.com>
2021-12-08 06:46:34 +0900

    위치정보 추출해서 텍스트로 표시

--- MapPractice/MapPractice/ViewController.swift
+++ MapPractice/MapPractice/ViewController.swift
@@ -59,25 +59,7 @@ extension ViewController {
         locationManager.startUpdatingLocation()
     }
 
-    // 위도, 경도, 범위 -> 원하는 위치 표시하는 함수
-    private func goLocation(latitudeValue: CLLocationDegrees, longitudeValue: CLLocationDegrees, delta span: Double) {
-        // 위도 값과 경도 값을 매개변수
-        let pLocation = CLLocationCoordinate2DMake(latitudeValue, longitudeValue)
-        // 범위 값을 매개변수로
-        let spanValue = MKCoordinateSpan(latitudeDelta: span, longitudeDelta: span)
-        let pRegion = MKCoordinateRegion(center: pLocation, span: spanValue)
-        mapView.setRegion(pRegion, animated: true)
-    }
 
-    // 위치가 없데이트 되었을 때 지도에 위치를 나타내기 위한 함수
-    private func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        // 위치가 업데이트 되면 먼저 마지막 위치 값을 찾아냄
-        let pLocation = locations.last
-        /// 마지막 위치의 위도와 경도 값을 가지고 앞에서 만든 goLocation 함수 호출
-        /// delta 값은 지도의 크기. 값이 작을수록 확대되는 효과가 있음. 100배 확대 
-        goLocation(latitudeValue: (pLocation?.coordinate.latitude)! , longitudeValue: (pLocation?.coordinate.longitude)! , delta: 0.01 )
-
-    }
 }
 
 extension ViewController {
@@ -88,6 +70,52 @@ extension ViewController {
 
 // Delegate
 extension ViewController: CLLocationManagerDelegate {
+
+    // 위도, 경도, 범위 -> 원하는 위치 표시하는 함수
+    func goLocation(latitudeValue: CLLocationDegrees, longitudeValue: CLLocationDegrees, delta span: Double) {
+        // 위도 값과 경도 값을 매개변수
+        let pLocation = CLLocationCoordinate2DMake(latitudeValue, longitudeValue)
+        // 범위 값을 매개변수로
+        let spanValue = MKCoordinateSpan(latitudeDelta: span, longitudeDelta: span)
+        let pRegion = MKCoordinateRegion(center: pLocation, span: spanValue)
+        mapView.setRegion(pRegion, animated: true)
+    }
+
+    // 위치가 없데이트 되었을 때 지도에 위치를 나타내기 위한 함수
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        // 위치가 업데이트 되면 먼저 마지막 위치 값을 찾아냄
+        let pLocation = locations.last
+        /// 마지막 위치의 위도와 경도 값을 가지고 앞에서 만든 goLocation 함수 호출
+        /// delta 값은 지도의 크기. 값이 작을수록 확대되는 효과가 있음. 100배 확대
+        goLocation(latitudeValue: (pLocation?.coordinate.latitude)! , longitudeValue: (pLocation?.coordinate.longitude)! , delta: 0.01)
+
+        /// 위도와 경도 값을 가지고 역으로 위치 정보, 즉 주소 찾기
+        CLGeocoder().reverseGeocodeLocation(pLocation!, completionHandler: {
+            (placemarks, error) -> Void in
+            // placemarks의 첫부분만 pm에 저장
+            let pm = placemarks!.first
+            // pm 상수에서 나라 값을 대입
+            let country = pm!.country
+            // country 상수를 address 문자열에 대입
+            var address:String = country!
+            // pm에서 지역 값이 존재하면 address 문자열에 추가
+            if pm!.locality != nil {
+                address += " "
+                address += pm!.locality!
+            }
+            // 도로 값이 존재하면 추가
+            if pm!.thoroughfare != nil {
+                address += " "
+                address += pm!.thoroughfare!
+            }
+
+            self.titleLabel.text = "현재 위치"
+            self.locationLabel.text = address
+        })
+
+        // 위치 없데이트 멈추기
+        locationManager.stopUpdatingLocation()
+    }
 
 }
 
